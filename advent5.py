134503dbc4a9bc29b168fdea1362cfa5242ba27c
Preeti Ramaraj <preetir@umich.edu>
2017-12-10 22:50:28 -0500

    Advent 5 part 2

--- advent5.py
+++ advent5.py
@@ -7,10 +7,15 @@ inst = [int(x.replace("\n", "")) for x in inp]
 
 curr_pos = 0
 step = 0
-while curr_pos < len(inst):
+while 0 <= curr_pos < len(inst):
     prev_pos = curr_pos
     curr_pos += inst[prev_pos]
-    inst[prev_pos] += 1
+    # Part 2
+    if inst[prev_pos] >= 3:
+        inst[prev_pos] -= 1
+    else:
+        inst[prev_pos] += 1 #Part 1
     step += 1
-
+print "step"
 print step
+
