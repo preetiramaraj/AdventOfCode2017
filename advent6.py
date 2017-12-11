f112e82db6abb1615e3581e19795e44a9240ee24
Preeti Ramaraj <preetir@umich.edu>
2017-12-11 15:11:31 -0500

    Advent day 6 part 2

--- advent6.py
+++ advent6.py
@@ -1,9 +1,8 @@
 # Advent 6
 
 inp = "2	8	8	5	4	2	3	1	5	5	1	2	15	13	5	14"
-
 blocks = [int(x) for x in inp.split("\t")]
-
+#blocks = [0,2,7,0]
 str_list = [' '.join([str(x) for x in blocks])]
 curr = ''
 steps = 0
@@ -18,4 +17,6 @@ while curr not in str_list:
         value -= 1
     curr = ' '.join([str(x) for x in blocks])
     steps += 1
-print steps
+
+original_index = str_list.index(curr)
+print (steps-original_index+1) # +1 since you add '' at the beginning
