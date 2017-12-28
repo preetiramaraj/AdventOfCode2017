bc589d39ba25ca3886b494c1deeba71240743d66
Preeti Ramaraj <preetir@umich.edu>
2017-12-28 11:45:49 -0500

    Advent day 12 part 2

--- advent12.py
+++ advent12.py
@@ -3,9 +3,7 @@
 
 def find_indirect_connections(z, conn, contains_zero):
     curr_list = []
-    for y in z:
-        curr_list += [x for x in conn.keys() if y in conn[x] and [True for z in contains_zero if x in z] != [True]]
-   # print z, curr_list
+    curr_list += [x for x in conn.keys() if z in conn[x] and x not in contains_zero]
     if len(curr_list) == 0:
         return ['-1']
     else:
@@ -18,22 +16,27 @@ def main():
 
     input = [x.replace("\n", "").split("<->") for x in inp]
     conn = {}
-    contains_zero = [['0'], []]
     for i, val in enumerate(input):
         curr_key = val[0].replace(" ", "")
         curr_values = [x.replace(" ", "") for x in val[1].split(", ")]
-        if '0' in curr_values:
-            contains_zero[1].append(curr_key)
         conn[curr_key] = curr_values
-
-    i = 1
-    new_conn = []
-    while new_conn != ['-1']:
-        new_conn = find_indirect_connections(contains_zero[i], conn, contains_zero)
-        contains_zero.append(new_conn)
-        i = i+1
-    contains_zero.remove(['-1'])
-    print len([x for sublist in contains_zero for x in sublist])
+    all_nodes = list(conn.keys())
+    num_groups = 0
+    while len(all_nodes) > 0:
+        i = 0
+        contains_zero = [all_nodes[0]]
+        all_nodes.remove(contains_zero[0])
+        while i < len(contains_zero):
+            new_conn = find_indirect_connections(contains_zero[i], conn, contains_zero)
+            if new_conn == ['-1'] and i < len(contains_zero):
+                i = i+1
+                continue
+            contains_zero = contains_zero + new_conn
+            for x in new_conn:
+                all_nodes.remove(x)
+            i = i+1
+        num_groups += 1
+    print num_groups
 
 
 main()
